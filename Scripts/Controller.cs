88c6420988613a0f0e6e164b7c1bb921efe076aa
sogardLiahim <spireamihail@gmail.com>
2018-12-17 19:09:25 +0200

    Attach rigidbody to gameobjects, using joints with functional angular velocity to orbit around

---
+++ Scripts/Controller.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public GameObject planetToOrbit;
+    public Rigidbody2D rb;
+    public Rigidbody2D planetRb;
+
+
+    public float vinit = 0;
+    public float vfinal = 40f;
+    public float timeToAccelerate = 100f;
+    public float dt = 1.1f;
+    public float time = 0.0f;
+    Vector3 positionVector;
+    public float omega = 0.0f;
+    public  float alpha = 2f;
+    //public float TangentAcceleration = vfinal - vinit / timeToAccelerate;
+    public float TangentAcceleration = 0.1f;
+    Vector3 vectorPlanetPos;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        vectorPlanetPos = planetToOrbit.transform.position;
+        InvokeRepeating("TimeToIncrement", 0.1f, 0.01f);
+        jobSetRigidBodyToGameObject();
+
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+
+     
+     positionVector = planetToOrbit.transform.position - transform.position;
+
+        Vector3 velocityThatIsPerpendicular = new Vector3(-positionVector.y, positionVector.x, 0);
+        float mag = positionVector.magnitude;
+
+        float force = rb.inertia * omega;
+        Debug.Log(rb.inertia);
+
+
+        //Rotate to custom pivot
+        rb.angularVelocity = force;
+    }
+
+    void TimeToIncrement()
+    {
+        while (time < timeToAccelerate)
+        {
+            time += dt;
+            omega = alpha * time * time;
+            return;
+        }
+        TangentAcceleration = 0f;
+
+    }
+
+    void jobSetRigidBodyToGameObject()
+    {
+        try
+        {
+            HingeJoint2D hingeJoint;
+            hingeJoint = planetToOrbit.AddComponent<HingeJoint2D>();
+            hingeJoint.connectedBody = rb;
+            planetRb = planetToOrbit.AddComponent<Rigidbody2D>();
+            planetRb.gravityScale = 0;
+
+        }
+        catch { }
+    }
+
+    //Vector3 torqueForce = new Vector3(0, 0, torqueMag);
+    //angle = Vector3.Angle(positionVector, yAxis) + STEP;
+    //float torqueMag = force * mag * Mathf.Sin(angle); // the value of the torqueForce Mangnitudine
+
+
+    void Update ()
+    {
+		
+	}
+}
+
---
+++ Scripts/TorqueTry.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorqueTry : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject planetToOrbit;
+    public Rigidbody2D rb;
+
+    float speed = 30.0f;
+    float rotationSpeed = 10.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void fixedUpdate()
+    {
+        // Multiply speed with the input.
+        // var speed can be changed in the inspector
+        // without changing any scripts
+        Vector3 force = new Vector3(0, 0, 100f);
+        force = speed * force;
+        // Don’t multiply by Time.deltaTime
+        // since forces are already time independent.
+        // Apply force along the z axis of the object
+        rb.AddRelativeForce(force);
+        Vector3 torque = new Vector3(Input.GetAxis("Horizontal"), 0, 0); 
+        torque = rotationSpeed * torque;
+        // Rotate around the world y-axis
+        rb.AddTorque(torque.magnitude);
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
