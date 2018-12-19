cbf61f56172f6d75e8b952fa126d55cf818f33e9
sogardLiahim <spireamihail@gmail.com>
2018-12-19 18:58:19 +0200

    Before decelerate implementation.

--- Scripts/Controller.cs
+++ Scripts/Controller.cs
@@ -9,9 +9,8 @@ public class Controller : MonoBehaviour {
 
 
     public float vinit = 0;
-    public float vfinal = 40f;
     public float timeToAccelerate = 100f;
-    public float dt = 1.1f;
+    public float dt = 0.1f;
     public float time = 0.0f;
     Vector3 positionVector;
     public float omega = 0.0f;
@@ -19,11 +18,15 @@ public class Controller : MonoBehaviour {
     //public float TangentAcceleration = vfinal - vinit / timeToAccelerate;
     public float TangentAcceleration = 0.1f;
     Vector3 vectorPlanetPos;
+    private bool isInTrigger = false;
+
+  
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         vectorPlanetPos = planetToOrbit.transform.position;
-        InvokeRepeating("TimeToIncrement", 0.1f, 0.01f);
+        InvokeRepeating("TimeToIncrement", 0.1f, dt);
+        omega = vinit;
        // jobSetRigidBodyToGameObject();
 
     }
@@ -41,49 +44,33 @@ public class Controller : MonoBehaviour {
 
         float force = rb.inertia * omega;
         Debug.Log(rb.inertia);
+       
 
-
-        //Rotate to custom pivot
+        //Rotate to custom pivot, seeking hinge joint or just his local axis in motion
         rb.angularVelocity = force;
     }
 
     void TimeToIncrement()
     {
-        while (time < timeToAccelerate)
+        Debug.Log(rb.angularVelocity);
+        if (isInTrigger)
         {
-            time += dt;
-            omega = alpha * time * time;
-            return;
+            while (time < timeToAccelerate)
+            {
+                time += dt;
+                omega = (alpha * time * time);
+                return;
+            }
+        }
+        else
+        {
+            time = 0;
+            //decelerate.
         }
         TangentAcceleration = 0f;
 
     }
 
-    void jobSetRigidBodyToGameObject(GameObject planetToOrbit)
-    {
-        Rigidbody2D planetRb;
-        planetRb = planetToOrbit.AddComponent<Rigidbody2D>();
-        planetRb.gravityScale = 0;
-       //Add HingeJoint to specific gameObject;
-        HingeJoint2D hingeJoint;
-        hingeJoint = planetToOrbit.AddComponent<HingeJoint2D>();
-        hingeJoint.connectedBody = rb;
-    }
-
-    void jobDisableRigidBodyOnExit(GameObject planetToOrbit)
-    {
-        Destroy(planetToOrbit.GetComponent<HingeJoint2D>());
-        Destroy(planetToOrbit.GetComponent<Rigidbody2D>());
-    }
-
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        jobSetRigidBodyToGameObject(col.gameObject);
-    }
-    void OnTriggerExit2D(Collider2D col)
-    {
-        jobDisableRigidBodyOnExit(col.gameObject);
-    }
 
     //Vector3 torqueForce = new Vector3(0, 0, torqueMag);
     //angle = Vector3.Angle(positionVector, yAxis) + STEP;
@@ -92,7 +79,53 @@ public class Controller : MonoBehaviour {
 
     void Update ()
     {
-		
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            jobDisableRigidBodyOnExit(planetToOrbit);
+        }
+    }
+
+
+    void jobSetRigidBodyToGameObject(GameObject planetToOrbitLocal)
+    {
+        planetToOrbit = planetToOrbitLocal;
+        //Add Rigidbody2D to specific gameObject;
+        if (planetToOrbitLocal.GetComponent<Rigidbody2D>() == null)
+        {
+            Rigidbody2D planetRb;
+            planetRb = planetToOrbitLocal.AddComponent<Rigidbody2D>();
+            planetRb.gravityScale = 0;
+            planetRb.bodyType = RigidbodyType2D.Static;
+            //Add HingeJoint2D to specific gameObject;
+            HingeJoint2D hingeJoint;
+            hingeJoint = planetToOrbitLocal.AddComponent<HingeJoint2D>();
+            hingeJoint.connectedBody = rb;
+            isInTrigger = true;
+        }
+    }
+
+    void jobDisableRigidBodyOnExit(GameObject planetToOrbitLocal)
+    {
+        //Diable rigidbody and hinge components from specific GameObject
+        if (planetToOrbitLocal != null)
+        {
+            isInTrigger = false;
+            Destroy(planetToOrbitLocal.GetComponent<HingeJoint2D>());
+            Destroy(planetToOrbitLocal.GetComponent<Rigidbody2D>());
+            Destroy(planetToOrbitLocal.GetComponent<Collider2D>());
+        }
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Planet")
+        {
+            jobSetRigidBodyToGameObject(col.gameObject);
+
+        }
+
+    }
 }
 
